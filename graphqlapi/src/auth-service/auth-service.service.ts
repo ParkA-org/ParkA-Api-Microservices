63351f083d21ba7107ef4e89535a7f26a42a5bae
Sebastiano Faiella <xeno097.cp@gmail.com>
2020-09-15 13:27:16 -0400

    added logger example to auth-service.ts

--- graphqlapi/src/auth-service/auth-service.service.ts
+++ graphqlapi/src/auth-service/auth-service.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { Client, ClientProxy, Transport } from '@nestjs/microservices';
 import { UserType } from './auth-service.type';
 
 @Injectable()
 export class AuthServiceService {
+  private logger = new Logger('AuthServiceService');
   @Client({
     transport: Transport.REDIS,
     options: { url: `redis://redis-parka-microservices:6379` },
@@ -11,6 +12,7 @@ export class AuthServiceService {
   client: ClientProxy;
 
   public async getUserById(id: string): Promise<{}> {
+    this.logger.log(`Getting user`);
     const response = await this.client.send<{}>({ type: 'get-user' }, {});
     return response.toPromise();
   }
