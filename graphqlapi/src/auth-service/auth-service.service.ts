9175bad3038eba01ae57e1db1f68f4d3c9772b43
Sebastiano Faiella <xeno097.cp@gmail.com>
2020-09-15 12:02:50 -0400

    created base service

--- graphqlapi/src/auth-service/auth-service.service.ts
+++ graphqlapi/src/auth-service/auth-service.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { Client, ClientProxy, Transport } from '@nestjs/microservices';
+import { UserType } from './auth-service.type';
 
 @Injectable()
-export class AuthServiceService {}
+export class AuthServiceService {
+  @Client({
+    transport: Transport.REDIS,
+    options: { url: `redis://localhost:6379` },
+  })
+  client: ClientProxy;
+
+  public async getUserById(id: string): Promise<UserType> {
+    return { id: 'helloworld v:' };
+  }
+}
