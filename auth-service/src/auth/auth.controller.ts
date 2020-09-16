59310143255d85d65ab4496c0d79c71c9979b67a
Sebastiano Faiella <xeno097.cp@gmail.com>
2020-09-16 17:22:10 -0400

    created base user mutation resolver and entities

--- auth-service/src/auth/auth.controller.ts
+++ auth-service/src/auth/auth.controller.ts
@@ -1,8 +1,11 @@
 import { Controller } from '@nestjs/common';
 import { MessagePattern } from '@nestjs/microservices';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private authService: AuthService) {}
+
   @MessagePattern({ type: 'get-user' })
   public async getCatalogItems(): Promise<{}> {
     return { id: 'helloworld v:' };
--- auth-service/src/auth/userData/user.entity.ts
+++ auth-service/src/auth/userData/user.entity.ts
@@ -3,7 +3,7 @@ import { Column, Entity, PrimaryColumn } from 'typeorm';
 @Entity()
 export class User {
   @PrimaryColumn()
-  userId: string;
+  id: string;
 
   @Column()
   name: string;
