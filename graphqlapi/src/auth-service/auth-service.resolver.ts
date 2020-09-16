223ac8586e833aaa24f63c32b1aab555f272b852
Sebastiano Faiella <xeno097.cp@gmail.com>
2020-09-16 17:40:41 -0400

    created createUser method in auth service of graphql api

--- graphqlapi/src/auth-service/auth-service.resolver.ts
+++ graphqlapi/src/auth-service/auth-service.resolver.ts
@@ -1,6 +1,7 @@
 import { Query, Resolver, Mutation, Args } from '@nestjs/graphql';
 
 import { AuthServiceService } from './auth-service.service';
+import { CreateUserInput } from './user-data/user.input';
 import { UserType } from './user-data/user.type';
 
 @Resolver(of => UserType)
@@ -13,7 +14,9 @@ export class AuthServiceResolver {
   }
 
   @Mutation(returns => UserType)
-  async createUser(): Promise<UserType> {
+  async createUser(
+    @Args('createUserInput') createUserInput: CreateUserInput,
+  ): Promise<UserType> {
     return await {
       id: '12345',
       name: 'test name',
--- graphqlapi/src/auth-service/auth-service.service.ts
+++ graphqlapi/src/auth-service/auth-service.service.ts
@@ -1,10 +1,12 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Client, ClientProxy, Transport } from '@nestjs/microservices';
-import { UserType } from './auth-service.type';
+import { CreateUserInput } from './user-data/user.input';
+import { UserType } from './user-data/user.type';
 
 @Injectable()
 export class AuthServiceService {
   private logger = new Logger('AuthServiceService');
+
   @Client({
     transport: Transport.REDIS,
     options: { url: `redis://redis-parka-microservices:6379` },
@@ -14,6 +16,15 @@ export class AuthServiceService {
   public async getUserById(id: string): Promise<{}> {
     this.logger.log(`Getting user`);
     const response = await this.client.send<{}>({ type: 'get-user' }, {});
+    return response.toPromise();
+  }
+
+  public async createUser(createUserInput: CreateUserInput): Promise<UserType> {
+    this.logger.log(`Got createUserInput data`);
+    const response = this.client.send<UserType>(
+      { type: 'create-user' },
+      createUserInput,
+    );
     return response.toPromise();
   }
 }
