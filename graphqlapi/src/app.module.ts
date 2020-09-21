5cddd00cd27b53fbe858551d9a1a9eeccb7ed2ac
Sebastiano Faiella <xeno097.cp@gmail.com>
2020-09-21 17:42:12 -0400

    added vehicle-service to graphql api

--- graphqlapi/src/app.module.ts
+++ graphqlapi/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AuthServiceModule } from './auth-service/auth-service.module';
 import { GraphQLModule } from '@nestjs/graphql';
+import { VehicleServiceModule } from './vehicle-service/vehicle-service.module';
 @Module({
-  imports: [AuthServiceModule, GraphQLModule.forRoot({ autoSchemaFile: true })],
+  imports: [AuthServiceModule, GraphQLModule.forRoot({ autoSchemaFile: true }), VehicleServiceModule],
 })
 export class AppModule {}
