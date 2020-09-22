6e1d321a4b427950e2fda992d4ae6546c3a0f85d
Sebastiano Faiella <xeno097.cp@gmail.com>
2020-09-22 14:10:55 -0400

    completed basic logic to create and find one make

--- graphqlapi/src/vehicle-service/vehicle-make/vehicle-make-inputs/create-vehicle-make.input.ts
+++ graphqlapi/src/vehicle-service/vehicle-make/vehicle-make-inputs/create-vehicle-make.input.ts
@@ -1,14 +1,17 @@
 import { Field, ID, InputType } from '@nestjs/graphql';
+import { IsUUID, MinLength } from 'class-validator';
 import { ICreateVehicleMakeInput } from '../vehicle-make-interfaces/create-make-input.interface';
 
 @InputType('createVehicleMakeInput')
 export class CreateVehicleMakeInput implements ICreateVehicleMakeInput {
   @Field()
+  @MinLength(4)
   name: string;
 
   @Field()
   icon: string;
 
   @Field(type => [ID])
+  @IsUUID('4', { each: true })
   models: string[];
 }
