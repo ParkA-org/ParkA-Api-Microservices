2a5278edec75813c50e44512852dc501ac897e1e
Sebastiano Faiella <xeno097.cp@gmail.com>
2020-09-22 13:22:57 -0400

    created client property in vehicle-make

--- graphqlapi/src/vehicle-service/vehicle-make/vehicle-make.service.ts
+++ graphqlapi/src/vehicle-service/vehicle-make/vehicle-make.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { Client, ClientProxy, Transport } from '@nestjs/microservices';
 
 @Injectable()
-export class VehicleMakeService {}
+export class VehicleMakeService {
+  @Client({
+    transport: Transport.REDIS,
+    options: {
+      url: `redis://redis-parka-microservices:6379`,
+    },
+  })
+  private client: ClientProxy;
+}
