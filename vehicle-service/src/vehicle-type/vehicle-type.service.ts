eb0bd1610942baa05361be4c6abaa3340fbf7042
Sebastiano Faiella <xeno097.cp@gmail.com>
2020-09-21 22:31:44 -0400

    created basic crud operations delete excluded

--- vehicle-service/src/vehicle-type/vehicle-type.service.ts
+++ vehicle-service/src/vehicle-type/vehicle-type.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { VehicleType } from './vehicle-type-data/vehicle-type.entity';
 
 @Injectable()
-export class VehicleTypeService {}
+export class VehicleTypeService {
+  constructor(
+    @InjectRepository(VehicleType)
+    private vehicleTypeRepository: Repository<VehicleType>,
+  ) {}
+
+  //TODO: Define dto input type
+  public async getVehicleTypeById(): Promise<VehicleType> {
+    return this.vehicleTypeRepository.findOne();
+  }
+
+  public async getAllVehicleTypes(): Promise<VehicleType[]> {
+    return this.vehicleTypeRepository.find();
+  }
+
+  //TODO: define createVehicleType Dto
+  public async createVehicleType(): Promise<VehicleType> {
+    const {} = {};
+
+    return this.vehicleTypeRepository.save({});
+  }
+}
