01f2b1b0b290603ee4352e9025d021d8ac4b35bd
Sebastiano Faiella <xeno097.cp@gmail.com>
2020-09-22 13:08:47 -0400

    created make controller

--- vehicle-service/src/make/make.controller.ts
+++ vehicle-service/src/make/make.controller.ts
@@ -1,4 +1,21 @@
 import { Controller } from '@nestjs/common';
+import { MessagePattern } from '@nestjs/microservices';
+import { Make } from './make-data/make.entity';
+import { CreateMakeDto } from './make-dto/create-make.dto';
+import { GetMakeByIdDto } from './make-dto/get-make-by-id.dto';
+import { MakeService } from './make.service';
 
 @Controller('make')
-export class MakeController {}
+export class MakeController {
+  constructor(private makeService: MakeService) {}
+
+  @MessagePattern({ type: 'get-make-by' })
+  public async getMakeById(getMakeByIdDto: GetMakeByIdDto): Promise<Make> {
+    return await this.makeService.getMakeById(getMakeByIdDto);
+  }
+
+  @MessagePattern({ type: 'create-make' })
+  public async createMake(createMakeDto: CreateMakeDto): Promise<Make> {
+    return await this.makeService.createMake(createMakeDto);
+  }
+}
