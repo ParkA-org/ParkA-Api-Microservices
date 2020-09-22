ca0f3bac889d057b2bc393787799397ffe0fcdc2
Sebastiano Faiella <xeno097.cp@gmail.com>
2020-09-22 12:00:45 -0400

    added vehiclemodelmodule to main app module

--- graphqlapi/src/app.module.ts
+++ graphqlapi/src/app.module.ts
@@ -3,12 +3,14 @@ import { AuthServiceModule } from './auth-service/auth-service.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { VehicleServiceModule } from './vehicle-service/vehicle/vehicle-service.module';
 import { VehicleTypeModule } from './vehicle-service/vehicle-type/vehicle-type.module';
+import { VehicleModelModule } from './vehicle-service/vehicle-model/vehicle-model.module';
 @Module({
   imports: [
     GraphQLModule.forRoot({ autoSchemaFile: true }),
     VehicleServiceModule,
     AuthServiceModule,
     VehicleTypeModule,
+    VehicleModelModule,
   ],
 })
 export class AppModule {}
