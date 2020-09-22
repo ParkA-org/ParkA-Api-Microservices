53b63dfcdb09a4d62a9ae6837156af0eda131342
Sebastiano Faiella <xeno097.cp@gmail.com>
2020-09-22 11:56:21 -0400

    created create vehicle model resolver in graphql api

--- graphqlapi/src/vehicle-service/vehicle-model/vehicle-model.resolver.ts
+++ graphqlapi/src/vehicle-service/vehicle-model/vehicle-model.resolver.ts
@@ -1,5 +1,6 @@
-import { Args, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { VehicleModelType } from './vehicle-model-data/vehicle-model.type';
+import { CreateVehicleModelInput } from './vehicle-model-inputs/create-vehicle-model.input';
 import { GetVehicleModelByIdInput } from './vehicle-model-inputs/get-vehicle-model-by-id.input';
 import { VehicleModelService } from './vehicle-model.service';
 
@@ -9,10 +10,21 @@ export class VehicleModelResolver {
 
   @Query(returns => VehicleModelType)
   public async getVehicleModelById(
-    @Args() getVehicleModelByIdInput: GetVehicleModelByIdInput,
+    @Args('getVehicleModelByIdInput')
+    getVehicleModelByIdInput: GetVehicleModelByIdInput,
   ) {
     return await this.vehicleModelService.getVehicleModelById(
       getVehicleModelByIdInput,
+    );
+  }
+
+  @Mutation(of => VehicleModelType)
+  public async createVehicleModel(
+    @Args('createVehicleModelInput')
+    createVehicleModelInput: CreateVehicleModelInput,
+  ): Promise<VehicleModelType> {
+    return await this.vehicleModelService.createVehicleModel(
+      createVehicleModelInput,
     );
   }
 }
