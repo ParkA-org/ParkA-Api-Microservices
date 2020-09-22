5b2edf27c8962b6d0422c2aa60f476cc68757607
Sebastiano Faiella <xeno097.cp@gmail.com>
2020-09-22 11:09:40 -0400

    created get model by id handler in the model controller

--- vehicle-service/src/model/model.controller.ts
+++ vehicle-service/src/model/model.controller.ts
@@ -1,4 +1,15 @@
 import { Controller } from '@nestjs/common';
+import { MessagePattern } from '@nestjs/microservices';
+import { Model } from './model-data/model.entity';
+import { GetModelByIdDto } from './model-dto/get-model-by-id.dto';
+import { ModelService } from './model.service';
 
 @Controller('model')
-export class ModelController {}
+export class ModelController {
+  constructor(private modelService: ModelService) {}
+
+  @MessagePattern({ type: 'get-model-by-id' })
+  public async getModelById(getModelByIdDto: GetModelByIdDto): Promise<Model> {
+    return await this.modelService.getModelById(getModelByIdDto);
+  }
+}
