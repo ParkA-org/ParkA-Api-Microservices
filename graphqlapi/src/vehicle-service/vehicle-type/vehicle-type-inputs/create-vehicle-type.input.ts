0f9b6b148ff469f6b957f15f3b5d34224ed01175
Sebastiano Faiella <xeno097.cp@gmail.com>
2020-09-21 23:59:37 -0400

    added validation to create vehicle type input

--- graphqlapi/src/vehicle-service/vehicle-type/vehicle-type-inputs/create-vehicle-type.input.ts
+++ graphqlapi/src/vehicle-service/vehicle-type/vehicle-type-inputs/create-vehicle-type.input.ts
@@ -1,8 +1,10 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { MinLength } from 'class-validator';
 import { ICreateVehicleTypeInput } from '../vehicle-type-interfaces/create-vehicle-type-input.interface';
 
 @InputType('createVehicleTypeInput')
 export class CreateVehicleTypeInput implements ICreateVehicleTypeInput {
   @Field()
+  @MinLength(2)
   name: string;
 }
