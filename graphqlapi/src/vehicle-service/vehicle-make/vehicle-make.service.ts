5ea4ea180912fd1c042a1c8cb8c8cab875f77e21
Sebastiano Faiella <xeno097.cp@gmail.com>
2020-09-22 13:44:16 -0400

    created method to create make in graphql api

--- graphqlapi/src/vehicle-service/vehicle-make/vehicle-make.service.ts
+++ graphqlapi/src/vehicle-service/vehicle-make/vehicle-make.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Client, ClientProxy, Transport } from '@nestjs/microservices';
 import { GetVehicleModelByIdInput } from '../vehicle-model/vehicle-model-inputs/get-vehicle-model-by-id.input';
+import { CreateVehicleMakeInput } from './vehicle-make-inputs/create-vehicle-make.input';
 import { VehicleMakeType } from './vehicle-make-type/vehicle-make.type';
 
 @Injectable()
@@ -19,6 +20,19 @@ export class VehicleMakeService {
     const response = await this.client.send<VehicleMakeType>(
       { type: 'get-make-by-id' },
       getVehicleMakeByIdInput,
+    );
+
+    return response.toPromise();
+  }
+
+  public async createVehicleMake(
+    createVehicleMakeInput: CreateVehicleMakeInput,
+  ): Promise<VehicleMakeType> {
+    const response = await this.client.send<VehicleMakeType>(
+      {
+        type: 'create-make',
+      },
+      createVehicleMakeInput,
     );
 
     return response.toPromise();
