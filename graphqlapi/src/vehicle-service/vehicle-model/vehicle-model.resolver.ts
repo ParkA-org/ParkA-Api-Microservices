c918780f3ce3ab5e6b2f1d8582d3de156c9c6280
Sebastiano Faiella <xeno097.cp@gmail.com>
2020-09-22 11:52:53 -0400

    created get vehicle by id resolver in graphql api

--- graphqlapi/src/vehicle-service/vehicle-model/vehicle-model.resolver.ts
+++ graphqlapi/src/vehicle-service/vehicle-model/vehicle-model.resolver.ts
@@ -1,4 +1,18 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver } from '@nestjs/graphql';
+import { VehicleModelType } from './vehicle-model-data/vehicle-model.type';
+import { GetVehicleModelByIdInput } from './vehicle-model-inputs/get-vehicle-model-by-id.input';
+import { VehicleModelService } from './vehicle-model.service';
 
 @Resolver()
-export class VehicleModelResolver {}
+export class VehicleModelResolver {
+  constructor(private vehicleModelService: VehicleModelService) {}
+
+  @Query(returns => VehicleModelType)
+  public async getVehicleModelById(
+    @Args() getVehicleModelByIdInput: GetVehicleModelByIdInput,
+  ) {
+    return await this.vehicleModelService.getVehicleModelById(
+      getVehicleModelByIdInput,
+    );
+  }
+}
