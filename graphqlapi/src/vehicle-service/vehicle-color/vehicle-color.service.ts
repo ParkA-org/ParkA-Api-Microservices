a00ed09dd173d2ca18615a6dcdbba0ee53b2f9d1
Sebastiano Faiella <xeno097.cp@gmail.com>
2020-09-22 17:21:37 -0400

    created color cr operations in graphql api

--- graphqlapi/src/vehicle-service/vehicle-color/vehicle-color.service.ts
+++ graphqlapi/src/vehicle-service/vehicle-color/vehicle-color.service.ts
@@ -1,4 +1,53 @@
 import { Injectable } from '@nestjs/common';
+import { Client, ClientProxy, Transport } from '@nestjs/microservices';
+import { CreateVehicleColorInput } from './vehicle-color-inputs/create-vehicle-color.input';
+import { GetVehicleColorByIdInput } from './vehicle-color-inputs/get-vehicle-color-by-id.input';
+import { VehicleColorType } from './vehicle-color-type/vehicle-color.type';
 
 @Injectable()
-export class VehicleColorService {}
+export class VehicleColorService {
+  @Client({
+    transport: Transport.REDIS,
+    options: {
+      url: `redis://redis-parka-microservices:6379`,
+    },
+  })
+  private client: ClientProxy;
+
+  public async getVehicleColorById(
+    getVehicleColorByIdInput: GetVehicleColorByIdInput,
+  ): Promise<VehicleColorType> {
+    const response = this.client.send<VehicleColorType>(
+      {
+        type: 'get-color-by-id',
+      },
+      getVehicleColorByIdInput,
+    );
+
+    return await response.toPromise();
+  }
+
+  public async getAllVehicleColors(): Promise<VehicleColorType[]> {
+    const response = this.client.send<VehicleColorType[]>(
+      {
+        type: 'get-all-colors',
+      },
+      {},
+    );
+
+    return await response.toPromise();
+  }
+
+  public async createVehicleColor(
+    createVehicleColorInput: CreateVehicleColorInput,
+  ): Promise<VehicleColorType> {
+    const response = this.client.send<VehicleColorType>(
+      {
+        type: 'create-color',
+      },
+      createVehicleColorInput,
+    );
+
+    return await response.toPromise();
+  }
+}
