6a7ce48fd08ec58125607d3790a1b19612f73b1d
Sebastiano Faiella <xeno097.cp@gmail.com>
2020-09-22 11:40:46 -0400

    created getVehicleByIdInput object

---
+++ graphqlapi/src/vehicle-service/vehicle-model/vehicle-model-inputs/get-vehicle-model-by-id.input.ts
@@ -0,0 +1,10 @@
+import { Field, InputType } from '@nestjs/graphql';
+import { IsUUID } from 'class-validator';
+import { IGetVehicleByIdInput } from 'src/vehicle-service/vehicle/interfaces/get-vehicle-by-id.interface';
+
+@InputType('getVehicleModelByIdInput')
+export class GetVehicleModelById implements IGetVehicleByIdInput {
+  @Field()
+  @IsUUID('4')
+  id: string;
+}
