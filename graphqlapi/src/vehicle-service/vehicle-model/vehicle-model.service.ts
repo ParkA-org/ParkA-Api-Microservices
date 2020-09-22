a66defdeaf6bc15d4e2c2e645de68d9bb425888c
Sebastiano Faiella <xeno097.cp@gmail.com>
2020-09-22 11:49:06 -0400

    created create vehicle model method in api gateway

--- graphqlapi/src/vehicle-service/vehicle-model/vehicle-model.service.ts
+++ graphqlapi/src/vehicle-service/vehicle-model/vehicle-model.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Client, ClientProxy, Transport } from '@nestjs/microservices';
 import { VehicleModelType } from './vehicle-model-data/vehicle-model.type';
+import { CreateVehicleModelInput } from './vehicle-model-inputs/create-vehicle-model.input';
 import { GetVehicleModelByIdInput } from './vehicle-model-inputs/get-vehicle-model-by-id.input';
 
 @Injectable()
@@ -16,13 +17,26 @@ export class VehicleModelService {
   public async getVehicleModelById(
     getVehicleModelByIdInput: GetVehicleModelByIdInput,
   ) {
-    const response = this.client.send<VehicleModelType>(
+    const response = await this.client.send<VehicleModelType>(
       {
         type: 'get-model-by-id',
       },
       getVehicleModelByIdInput,
     );
 
-    return await response.toPromise();
+    return response.toPromise();
+  }
+
+  public async createVehicleModel(
+    createVehicleModelInput: CreateVehicleModelInput,
+  ): Promise<VehicleModelType> {
+    const response = await this.client.send<VehicleModelType>(
+      {
+        type: 'create-model',
+      },
+      createVehicleModelInput,
+    );
+
+    return response.toPromise();
   }
 }
