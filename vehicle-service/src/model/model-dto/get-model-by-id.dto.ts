b3464945322b629638e71c122b7e5d6c8dbc8c79
Sebastiano Faiella <xeno097.cp@gmail.com>
2020-09-22 11:04:57 -0400

    created methods to get model by id and create a model

--- vehicle-service/src/model/model-dto/get-model-by-id.dto.ts
+++ vehicle-service/src/model/model-dto/get-model-by-id.dto.ts
@@ -1,7 +1,7 @@
 import { IsUUID } from 'class-validator';
 import { IGetModelByIdDto } from '../vehicle-interfaces/get-model-by-id-dto.interface';
 
-export class GetModelByIsDto implements IGetModelByIdDto {
+export class GetModelByIdDto implements IGetModelByIdDto {
   @IsUUID('4')
   id: string;
 }
--- vehicle-service/src/model/model.service.ts
+++ vehicle-service/src/model/model.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Model } from './model-data/model.entity';
+import { CreateModelDto } from './model-dto/create-model.dto';
+import { GetModelByIdDto } from './model-dto/get-model-by-id.dto';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class ModelService {
@@ -9,7 +12,21 @@ export class ModelService {
     @InjectRepository(Model) private modelRepository: Repository<Model>,
   ) {}
 
-  public async getModelById(): Promise<Model> {
-    return await this.modelRepository.findOne({});
+  public async getModelById(getModelByIdDto: GetModelByIdDto): Promise<Model> {
+    return await this.modelRepository.findOne(getModelByIdDto);
+  }
+
+  public async createModelById(createModelDto: CreateModelDto): Promise<Model> {
+    const { make, name } = createModelDto;
+
+    const model = this.modelRepository.create({
+      id: uuid(),
+      make,
+      name,
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString(),
+    });
+
+    return await this.modelRepository.save(model);
   }
 }
