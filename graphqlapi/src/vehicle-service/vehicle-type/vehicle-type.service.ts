911bbd6da28dedecc357d48826cf220b448542a8
Sebastiano Faiella <xeno097.cp@gmail.com>
2020-09-21 23:43:15 -0400

    created cr operations for vehicle type in graphql api

--- graphqlapi/src/vehicle-service/vehicle-type/vehicle-type.service.ts
+++ graphqlapi/src/vehicle-service/vehicle-type/vehicle-type.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Client, ClientProxy, Transport } from '@nestjs/microservices';
 import { VehicleTypeType } from './vehicle-type-data/vehicle-type.type';
+import { CreateVehicleTypeInput } from './vehicle-type-inputs/create-vehicle-type.input';
 import { GetVehicleTypeByIdInput } from './vehicle-type-inputs/get-vehicle-type-by-id.input';
 
 @Injectable()
@@ -17,8 +18,28 @@ export class VehicleTypeService {
     getVehicleTypeByIdInput: GetVehicleTypeByIdInput,
   ): Promise<VehicleTypeType> {
     const response = await this.client.send<VehicleTypeType>(
-      { type: 'get-vehicle' },
+      { type: 'get-vehicle-type-by-id' },
       getVehicleTypeByIdInput,
+    );
+
+    return response.toPromise();
+  }
+
+  public async getAllVehicleTypes(): Promise<VehicleTypeType> {
+    const response = await this.client.send<VehicleTypeType>(
+      { type: 'get-vehicle-all-types' },
+      {},
+    );
+
+    return response.toPromise();
+  }
+
+  public async createVehicleType(
+    createVehicleTypeInput: CreateVehicleTypeInput,
+  ): Promise<VehicleTypeType> {
+    const response = await this.client.send<VehicleTypeType>(
+      { type: 'create-vehicle-all-types' },
+      createVehicleTypeInput,
     );
 
     return response.toPromise();
