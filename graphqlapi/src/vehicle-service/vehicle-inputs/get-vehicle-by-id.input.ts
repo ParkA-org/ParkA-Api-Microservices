66c199a51a4f4098ff529b83d68216de40622bb9
Sebastiano Faiella <xeno097.cp@gmail.com>
2020-09-21 22:11:11 -0400

    added interface to create car input

--- graphqlapi/src/vehicle-service/vehicle-inputs/get-vehicle-by-id.input.ts
+++ graphqlapi/src/vehicle-service/vehicle-inputs/get-vehicle-by-id.input.ts
@@ -2,7 +2,7 @@ import { Field, InputType } from '@nestjs/graphql';
 import { IsUUID } from 'class-validator';
 import { IGetVehicleByIdInput } from '../interfaces/get-vehicle-by-id.interface';
 
-@InputType('findVehicleByIdInput')
+@InputType('getVehicleByIdInput')
 export class GetVehicleByIdInput implements IGetVehicleByIdInput {
   @IsUUID('4')
   @Field()
