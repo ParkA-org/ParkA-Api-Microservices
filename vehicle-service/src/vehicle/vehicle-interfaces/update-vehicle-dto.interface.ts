1c2d4e90a68590e335270c98032749a585c55979
Sebastiano Faiella <xeno097.cp@gmail.com>
2020-09-23 19:41:47 -0400

    created update vehicle dto

---
+++ vehicle-service/src/vehicle/vehicle-interfaces/update-vehicle-dto.interface.ts
@@ -0,0 +1,13 @@
+export interface IUpdateVehicleDto {
+  id: string;
+  model: string;
+  licensePlate: string;
+  verified: boolean;
+  detail: string;
+  colorExterior: string;
+  mainPicture: string;
+  pictures: string[];
+  year: string;
+  alias: string;
+  vehicleType: string;
+}
