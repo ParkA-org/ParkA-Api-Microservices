3eb134fa9c4b84f1149afc941f09359c99309e4a
Sebastiano Faiella <xeno097.cp@gmail.com>
2020-09-23 19:35:33 -0400

    added missing id validation in get-vehicle-by-id

--- vehicle-service/src/vehicle/vehicle-dto/get-vehicle-by-id.dto.ts
+++ vehicle-service/src/vehicle/vehicle-dto/get-vehicle-by-id.dto.ts
@@ -1,5 +1,7 @@
+import { IsUUID } from 'class-validator';
 import { IGetVehicleByIdDto } from '../vehicle-interfaces/get-vehicle-by-id-dto.interface';
 
 export class GetVehicleByIdDto implements IGetVehicleByIdDto {
+  @IsUUID('4')
   id: string;
 }
