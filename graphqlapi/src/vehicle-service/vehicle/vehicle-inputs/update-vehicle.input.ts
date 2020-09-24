25498d804a212adf633f61b5d5b215b1fc5d7c02
Sebastiano Faiella <xeno097.cp@gmail.com>
2020-09-23 22:51:11 -0400

    added update field decorator to update vehicle input

--- graphqlapi/src/vehicle-service/vehicle/vehicle-inputs/update-vehicle.input.ts
+++ graphqlapi/src/vehicle-service/vehicle/vehicle-inputs/update-vehicle.input.ts
@@ -1,32 +1,44 @@
+import { Field, InputType } from '@nestjs/graphql';
 import { IsUUID, MaxLength, MinLength } from 'class-validator';
 import { IUpdateVehicleInput } from '../interfaces/update-vehicle-input.interface';
 
+@InputType('UpdateVehicleInput')
 export class UpdateVehicleInput implements IUpdateVehicleInput {
   @IsUUID('4')
+  @Field()
   id: string;
 
+  @Field({ nullable: true })
   alias: string;
 
+  @Field({ nullable: true })
   @IsUUID('4')
   colorExterior: string;
 
+  @Field({ nullable: true })
   detail: string;
 
+  @Field({ nullable: true })
   @MaxLength(7)
   @MinLength(7)
   licensePlate: string;
 
+  @Field()
   mainPicture: string;
 
+  @Field({ nullable: true })
   @IsUUID('4')
   model: string;
   pictures: string[];
 
+  @Field({ nullable: true })
   @IsUUID('4')
   vehicleType: string;
 
+  @Field()
   verified: boolean;
 
+  @Field({ nullable: true })
   @MaxLength(4)
   @MinLength(4)
   year: string;
