bb60c712f71fc33a57ddf598ba74dd7ae828df26
Sebastiano Faiella <xeno097.cp@gmail.com>
2020-09-24 23:03:54 -0400

    added logger to make service

--- vehicle-service/src/make/make.service.ts
+++ vehicle-service/src/make/make.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Make } from './make-entities/make.entity';
@@ -6,22 +6,39 @@ import { CreateMakeDto } from './make-dto/create-make.dto';
 import { GetMakeByIdDto } from './make-dto/get-make-by-id.dto';
 import { v4 as uuid } from 'uuid';
 import { UpdateCarModelListDto } from './make-dto/update-car-model-list.dto';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class MakeService {
+  private logger = new Logger('MakeService');
+
   constructor(
     @InjectRepository(Make) private makeRepository: Repository<Make>,
   ) {}
 
   public async getMakeById(getMakeByIdDto: GetMakeByIdDto): Promise<Make> {
-    return this.makeRepository.findOne(getMakeByIdDto);
+    this.logger.debug(
+      `Received get make by id with payload ${JSON.stringify(getMakeByIdDto)}`,
+    );
+
+    const result = await this.makeRepository.findOne(getMakeByIdDto);
+
+    if (!result) {
+      throw new RpcException('Entry not found');
+    }
+
+    return result;
   }
 
   public async getAllMakes(): Promise<Make[]> {
-    return await this.makeRepository.find();
+    this.logger.debug(`Received get all makes`);
+    return this.makeRepository.find();
   }
 
   public async createMake(createMakeDto: CreateMakeDto): Promise<Make> {
+    this.logger.debug(
+      `Received create make with payload ${JSON.stringify(createMakeDto)}`,
+    );
     const { icon, models, name } = createMakeDto;
 
     const make = this.makeRepository.create({
@@ -33,12 +50,17 @@ export class MakeService {
       updatedAt: new Date().toISOString(),
     });
 
-    return await this.makeRepository.save(make);
+    return this.makeRepository.save(make);
   }
 
   public async updateModelList(
     updateModelListDto: UpdateCarModelListDto,
   ): Promise<Make> {
+    this.logger.debug(
+      `Received update model list with payload ${JSON.stringify(
+        updateModelListDto,
+      )}`,
+    );
     const { makeId, modelId } = updateModelListDto;
 
     const make = await this.getMakeById({ id: makeId });
