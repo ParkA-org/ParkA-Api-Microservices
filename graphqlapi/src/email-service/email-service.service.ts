309df320c9c42018267d3cf89586c3634c2eb1fa
David <davidbujosa@gmail.com>
2020-09-30 09:18:29 -0400

    Add validate email code

--- graphqlapi/src/email-service/email-service.service.ts
+++ graphqlapi/src/email-service/email-service.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Client, ClientProxy, Transport } from '@nestjs/microservices';
 import { ConfirmEmailInput } from './inputs/confirm-email.input';
+import { ValidateEmailCodeInput } from './inputs/validate-email-code.input';
 import { ConfirmEmailType } from './types/confirm-email.type';
 
 @Injectable()
@@ -20,6 +21,17 @@ export class EmailServiceService {
     const response = this.client.send<ConfirmEmailType>(
       { type: 'resend-email' },
       confirmEmailInput,
+    );
+    return response.toPromise();
+  }
+
+  public async validateEmailCode(
+    validateEmailCodeInput: ValidateEmailCodeInput,
+  ): Promise<ConfirmEmailType> {
+    this.logger.log('Got validate email code');
+    const response = this.client.send<ConfirmEmailType>(
+      { type: 'validate-email-code' },
+      validateEmailCodeInput,
     );
     return response.toPromise();
   }
