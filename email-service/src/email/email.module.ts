aaff80716605bb0aab01cc614932576224699f08
David <davidbujosa@gmail.com>
2020-09-30 07:00:24 -0400

    Add in email module typeorm for feature imports

--- email-service/src/email/email.module.ts
+++ email-service/src/email/email.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { EmailService } from './email.service';
 import { EmailController } from './email.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { ConfirmEmail } from './entities/confirm-email.entity';
 
 @Module({
   providers: [EmailService],
-  controllers: [EmailController]
+  controllers: [EmailController],
+  imports: [TypeOrmModule.forFeature([User, ConfirmEmail])],
 })
 export class EmailModule {}
