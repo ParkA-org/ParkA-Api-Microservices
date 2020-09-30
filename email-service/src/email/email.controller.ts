0ce3a5f7bc7f9259380393e66ace8219fd25bf66
David <davidbujosa@gmail.com>
2020-09-30 06:57:35 -0400

    Add confirm email in controller

--- email-service/src/email/email.controller.ts
+++ email-service/src/email/email.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Logger } from '@nestjs/common';
+import { MessagePattern } from '@nestjs/microservices';
+import { EmailService } from './email.service';
+import { CreateConfirmEmailDto } from './dto/create-confirm-email.dto';
+import { ConfirmEmail } from './entities/confirm-email.entity';
 
 @Controller('email')
-export class EmailController {}
+export class EmailController {
+  private logger = new Logger('EmailController');
+  constructor(private emailService: EmailService) {}
+
+  @MessagePattern({ type: 'confirm-email' })
+  public async confirmEmail(
+    createConfirmEmailDto: CreateConfirmEmailDto,
+  ): Promise<ConfirmEmail> {
+    this.logger.debug(
+      `Received confirm email message with data ${JSON.stringify(
+        createConfirmEmailDto,
+      )}`,
+    );
+    return await this.emailService.confirmEmail(createConfirmEmailDto);
+  }
+
+  // TODO Validate Code
+
+  // TODO Reset Password
+
+  // TODO Validate Reset Password
+}
