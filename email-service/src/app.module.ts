1422af5e4256897c632451915bbc393caa9be0f1
David <davidbujosa@gmail.com>
2020-09-30 04:27:08 -0400

    Add nodemailer, add controller, module, service

--- email-service/src/app.module.ts
+++ email-service/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EmailService } from './email/email.service';
+import { EmailModule } from './email/email.module';
+import { SModule } from './co/s/s.module';
+import { EmailModule } from './email/email.module';
 
 @Module({
-  imports: [],
+  imports: [EmailModule, SModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, EmailService],
 })
 export class AppModule {}
