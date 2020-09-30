4cf07e39db99f824d908c20c321daf485f7fcfad
David <davidbujosa@gmail.com>
2020-09-30 05:00:11 -0400

    Add new package and integrate TypeOrmModule

--- email-service/src/app.module.ts
+++ email-service/src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { EmailModule } from './email/email.module';
-import { EmailService } from './email/email.service';
 
 @Module({
-  imports: [EmailModule],
+  imports: [
+    EmailModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url:
+        'mongodb+srv://parkaApiUser:vUrmea2Sp4SSCBWj@parkawebapimicroservice.br7y0.mongodb.net/ParkaMicroservices?retryWrites=true&w=majority',
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      synchronize: true,
+      entities: [],
+    }),
+  ],
   controllers: [],
-  providers: [EmailService],
+  providers: [],
 })
 export class AppModule {}
