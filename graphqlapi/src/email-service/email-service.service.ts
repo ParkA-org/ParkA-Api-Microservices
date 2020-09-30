83dd8f308cb1e4fba80e184125b5e09b795b4f80
David <davidbujosa@gmail.com>
2020-09-30 06:29:01 -0400

    Create function confirmEmail in EmailServiceService

--- graphqlapi/src/email-service/email-service.service.ts
+++ graphqlapi/src/email-service/email-service.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { Client, ClientProxy, Transport } from '@nestjs/microservices';
+import { ConfirmEmailInput } from './inputs/confirm-email.input';
+import { ConfirmEmailType } from './types/confirm-email.type';
 
 @Injectable()
-export class EmailServiceService {}
+export class EmailServiceService {
+  private logger = new Logger('EmailServiceService');
+
+  @Client({
+    transport: Transport.REDIS,
+    options: { url: `redis://redis-parka-microservices:6379` },
+  })
+  client: ClientProxy;
+
+  public async confirmEmail(
+    confirmEmailInput: ConfirmEmailInput,
+  ): Promise<ConfirmEmailType> {
+    this.logger.log(`Got createConfirmEmail data`);
+    const response = this.client.send<ConfirmEmailType>(
+      { type: 'confirm-email' },
+      confirmEmailInput,
+    );
+    return response.toPromise();
+  }
+}
