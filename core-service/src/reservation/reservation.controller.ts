04e74bf4c24d50edb4d38210f088e35c31e840d8
Sebastiano Faiella <xeno097.cp@gmail.com>
2020-10-01 16:57:32 -0400

    created reservation service get all reservations method and relative controller

--- core-service/src/reservation/reservation.controller.ts
+++ core-service/src/reservation/reservation.controller.ts
@@ -1,4 +1,14 @@
 import { Controller } from '@nestjs/common';
+import { MessagePattern } from '@nestjs/microservices';
+import { Reservation } from './entities/reservation.entity';
+import { ReservationService } from './reservation.service';
 
 @Controller('reservation')
-export class ReservationController {}
+export class ReservationController {
+  constructor(private reservationService: ReservationService) {}
+
+  @MessagePattern({ type: 'get-all-reservations' })
+  public async getAllReservations(): Promise<Reservation[]> {
+    return this.reservationService.getAllReservations();
+  }
+}
--- core-service/src/reservation/reservation.service.ts
+++ core-service/src/reservation/reservation.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Reservation } from './entities/reservation.entity';
 
 @Injectable()
-export class ReservationService {}
+export class ReservationService {
+  constructor(
+    @InjectRepository(Reservation)
+    private reservationRepository: Repository<Reservation>,
+  ) {}
+
+  public async getAllReservations(): Promise<Reservation[]> {
+    return this.reservationRepository.find();
+  }
+}
