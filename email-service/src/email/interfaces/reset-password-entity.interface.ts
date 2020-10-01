bfe5862a2b616a5570c9ae313082d20caf85646d
David <davidbujosa@gmail.com>
2020-10-01 04:19:04 -0400

    Add reset Password Entity interface

---
+++ email-service/src/email/interfaces/reset-password-entity.interface.ts
@@ -0,0 +1,19 @@
+export interface IResetPassword {
+  _id: string;
+
+  id: string;
+
+  email: string;
+
+  origin: string;
+
+  code: string;
+
+  salt: string;
+
+  completed: boolean;
+
+  updatedAt: string;
+
+  createdAt: string;
+}
