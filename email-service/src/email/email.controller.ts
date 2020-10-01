a0bd5a27f6ad3f8c191a69be784f4e3b59042b4a
David <davidbujosa@gmail.com>
2020-10-01 05:27:44 -0400

    Add in email controller validate reset password code

--- email-service/src/email/email.controller.ts
+++ email-service/src/email/email.controller.ts
@@ -5,6 +5,8 @@ import { CreateConfirmEmailDto } from './dto/create-confirm-email.dto';
 import { ConfirmEmail } from './entities/confirm-email.entity';
 import { ValidateEmailCodeDto } from './dto/validate-email-code.dto';
 import { CreateResetPasswordDto } from './dto/create-reset-password.dto';
+import { ValidateResetPasswordCode } from './dto/validate-reset-password-code.dto';
+import { ResetPassword } from './entities/reset-password.entity';
 
 @Controller('email')
 export class EmailController {
@@ -47,16 +49,18 @@ export class EmailController {
     return await this.emailService.resendEmail(createConfirmEmailDto);
   }
 
-  @MessagePattern({ type: 'validate-email-code' })
-  public async validateEmailCode(
-    validateEmailCodeDto: ValidateEmailCodeDto,
-  ): Promise<ConfirmEmail> {
+  @MessagePattern({ type: 'validate-reset-password-code' })
+  public async validateResetPasswordCode(
+    validateResetPasswordCode: ValidateResetPasswordCode,
+  ): Promise<ResetPassword> {
     this.logger.debug(
-      `Received validate email code message with data ${JSON.stringify(
-        validateEmailCodeDto,
+      `Received validate reset password code message with data ${JSON.stringify(
+        validateResetPasswordCode,
       )}`,
     );
-    return await this.emailService.validateEmailCode(validateEmailCodeDto);
+    return await this.emailService.validateResetPasswordCode(
+      validateResetPasswordCode,
+    );
   }
 
   // TODO Validate Code
