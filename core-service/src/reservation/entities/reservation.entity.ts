513c0ea983b75dd8ce7ed005e80cd81ed2ab4a12
Sebastiano Faiella <xeno097.cp@gmail.com>
2020-10-01 16:32:08 -0400

    created reservation entity interface and class implementation

---
+++ core-service/src/reservation/entities/reservation.entity.ts
@@ -0,0 +1,11 @@
+class Reservation implements IReservation {
+  _id: string;
+  id: string;
+  checkInDate: string;
+  checkOutDate: string;
+  vehicleId: string;
+  paymentInfoId: string;
+  rentDate: string;
+  status: string;
+  userId: string;
+}
