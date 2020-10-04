01484a6f4e7a957cb480027e012f001384b6d933
David <davidbujosa@gmail.com>
2020-10-04 05:58:42 -0400

    Add config and module in app.module on payment services

--- payment-service/src/app.module.ts
+++ payment-service/src/app.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { PaymentServiceModule } from './payment-service/payment-service.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeorm.config';
 import { PaymentModule } from './payment/payment.module';
 
 @Module({
-  imports: [PaymentServiceModule, PaymentModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    PaymentModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot(typeOrmConfig),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
