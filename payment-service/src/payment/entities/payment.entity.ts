9dd7efc4e876697532452335fa11a63df8c9456c
David <davidbujosa@gmail.com>
2020-10-04 06:07:14 -0400

    Implement BaseEntity in PaymentServices

--- payment-service/src/payment/entities/payment.entity.ts
+++ payment-service/src/payment/entities/payment.entity.ts
@@ -1,3 +1,7 @@
+import { IBaseEntity } from '../interfaces/base-entity.interface';
 import { IPayment } from '../interfaces/payment-entity.interface';
 
-export class Payment implements IPayment {}
+export class Payment implements IPayment, IBaseEntity {
+  createdAt: string;
+  updatedAt: string;
+}
