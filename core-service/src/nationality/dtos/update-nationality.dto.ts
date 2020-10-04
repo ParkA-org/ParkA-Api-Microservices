12dddf0ab60acabd7ff31cfaf6db3de74abbdf46
Sebastiano Faiella <xeno097.cp@gmail.com>
2020-10-04 00:40:58 -0400

    created update nationality payload type in core service

--- core-service/src/nationality/dtos/update-nationality.dto.ts
+++ core-service/src/nationality/dtos/update-nationality.dto.ts
@@ -1,6 +1,7 @@
 import { GetNationalityById } from './get-nationality-by-id.dto';
+import { UpdateNationalityPayload } from './update-nationality-payload.dto';
 
 export class UpdateNationalityDto implements IUpdateNationalityDto {
   getNationalityByIdDto: GetNationalityById;
-  updateNationalityPayload: { name: string };
+  updateNationalityPayload: UpdateNationalityPayload;
 }
