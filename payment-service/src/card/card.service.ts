2e6d4ec593d110ee997fa4dbe7739aeb8eea1733
David <davidbujosa@gmail.com>
2020-10-05 05:32:52 -0400

    Add get card by id and create card in card service

--- payment-service/src/card/card.service.ts
+++ payment-service/src/card/card.service.ts
@@ -1,4 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { RpcException } from '@nestjs/microservices';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { GetCardDto } from './dtos/get-card.dto';
+import { Card } from './entities/card.entity';
+import { v4 as uuid } from 'uuid';
+import { CreateCardDto } from './dtos/create-card.dto';
 
 @Injectable()
-export class CardService {}
+export class CardService {
+  private logger = new Logger('CardService');
+
+  constructor(
+    @InjectRepository(Card) private cardRepository: Repository<Card>,
+  ) {}
+
+  public async getCardById(getCardDto: GetCardDto): Promise<Card> {
+    this.logger.debug(
+      `Received get card by id with payload ${JSON.stringify(getCardDto)}`,
+    );
+
+    const result = await this.cardRepository.findOne(getCardDto);
+
+    if (!result) {
+      throw new RpcException('Entry not found');
+    }
+
+    return result;
+  }
+
+  public async createCard(createCardDto: CreateCardDto): Promise<Card> {
+    this.logger.debug(
+      `Received create card with payload ${JSON.stringify(createCardDto)}`,
+    );
+
+    const { name } = createCardDto;
+
+    try {
+      const card = this.cardRepository.create({
+        id: uuid(),
+        name,
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString(),
+      });
+
+      return await this.cardRepository.save(card);
+    } catch (error) {
+      throw error.code === 11000
+        ? new RpcException('Duplicate field')
+        : new RpcException('An undefined error occured');
+    }
+  }
+}
