eacd36a5409953382db8dd36b282164fd91e140a
David <davidbujosa@gmail.com>
2020-10-05 04:28:58 -0400

    Add create and get card input

---
+++ graphqlapi/src/payment-service/card/inputs/create-card.input.ts
@@ -0,0 +1,10 @@
+import { Field, InputType } from '@nestjs/graphql';
+import { MinLength } from 'class-validator';
+import { ICreatedCardInput } from '../interfaces/create-card-input.interface';
+
+@InputType()
+export class CreateCardInput implements ICreatedCardInput {
+  @MinLength(2)
+  @Field()
+  name: string;
+}
---
+++ graphqlapi/src/payment-service/card/interfaces/get-card-by-id-input.interface.ts
@@ -0,0 +1,3 @@
+export interface IGetCardByIdInput {
+  id: string;
+}
