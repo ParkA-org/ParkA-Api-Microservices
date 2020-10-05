76bd118bf8e3cb9559d207627003fdabb3b59460
David <davidbujosa@gmail.com>
2020-10-05 04:34:09 -0400

    Finish card resolver

--- graphqlapi/src/payment-service/card/card.service.ts
+++ graphqlapi/src/payment-service/card/card.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { Client, ClientProxy, Transport } from '@nestjs/microservices';
+import { CreateCardInput } from './inputs/create-card.input';
+import { GetCardByIdInput } from './inputs/get-card-by-id.input';
+import { CardType } from './types/card.type';
 
 @Injectable()
-export class CardService {}
+export class CardService {
+  @Client({
+    transport: Transport.REDIS,
+    options: {
+      url: process.env.REDIS,
+    },
+  })
+  private client: ClientProxy;
+
+  public async createCard(createCardInput: CreateCardInput): Promise<CardType> {
+    const response = await this.client.send<CardType>(
+      { type: 'create-card' },
+      createCardInput,
+    );
+    return response.toPromise();
+  }
+
+  public async getCardById(
+    getCardByIdInput: GetCardByIdInput,
+  ): Promise<CardType> {
+    const response = await this.client.send<CardType>(
+      { type: 'get-payment' },
+      getCardByIdInput,
+    );
+    return response.toPromise();
+  }
+}
