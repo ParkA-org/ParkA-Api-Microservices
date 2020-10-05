88ca2d81d6ae6bcf27cbf3e5276dddb2ba0e9924
David <davidbujosa@gmail.com>
2020-10-05 04:45:26 -0400

    Create payment dto interface

--- payment-service/src/payment/payment.controller.ts
+++ payment-service/src/payment/payment.controller.ts
@@ -1,4 +1,15 @@
 import { Controller } from '@nestjs/common';
+import { MessagePattern } from '@nestjs/microservices';
+import { PaymentService } from './payment.service';
 
 @Controller('payment')
-export class PaymentController {}
+export class PaymentController {
+  constructor(private paymentService: PaymentService) {}
+  
+  @MessagePattern({ type: 'create-payment' })
+  public async createVehicle(
+    createVehicleDto: ,
+  ): Promise<Vehicle> {
+    return await this.vehicleService.createVehicle(createVehicleDto);
+  }
+}
