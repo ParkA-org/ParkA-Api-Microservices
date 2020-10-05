c730fb91797835d6ad44c39e6cc69cae7f0b8b2c
David <davidbujosa@gmail.com>
2020-10-05 04:19:11 -0400

    Finish payment module

--- graphqlapi/src/payment-service/payment/payment.module.ts
+++ graphqlapi/src/payment-service/payment/payment.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { CardModule } from '../card/card.module';
+import { PaymentResolver } from './payment.resolver';
+import { PaymentService } from './payment.service';
 
-@Module({})
+@Module({
+  imports: [CardModule],
+  providers: [PaymentResolver, PaymentService],
+})
 export class PaymentModule {}
