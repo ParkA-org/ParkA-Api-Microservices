f4cf01ad36acf24a1a2b95135d68ef7a2b0f7461
David <davidbujosa@gmail.com>
2020-10-05 05:16:44 -0400

    Finish payment entity

--- payment-service/src/payment/entities/payment.entity.ts
+++ payment-service/src/payment/entities/payment.entity.ts
@@ -1,17 +1,42 @@
+import { Column, Entity, ObjectIdColumn, PrimaryColumn } from 'typeorm';
 import { IBaseEntity } from '../interfaces/base-entity.interface';
 import { IPayment } from '../interfaces/payment-entity.interface';
 
+@Entity()
 export class Payment implements IPayment, IBaseEntity {
-  salt: string;
+  @PrimaryColumn()
   id: string;
+
+  @ObjectIdColumn()
   _id: string;
+
+  @Column()
   cardHolder: string;
+
+  @Column()
   digit: string;
+
+  @Column()
   expirationDate: string;
+
+  @Column()
+  salt: string;
+
+  @Column()
   cvv: string;
+
+  @Column()
   activated: boolean;
+
+  @Column()
   deleted: boolean;
+
+  @Column()
   card: string;
+
+  @Column()
   createdAt: string;
+
+  @Column()
   updatedAt: string;
 }
