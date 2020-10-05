6119cd6a3fa0463bffb208812116915553510943
David <davidbujosa@gmail.com>
2020-10-05 04:50:49 -0400

    Add get payment by id

--- payment-service/src/payment/payment.controller.ts
+++ payment-service/src/payment/payment.controller.ts
@@ -1,15 +1,16 @@
 import { Controller } from '@nestjs/common';
 import { MessagePattern } from '@nestjs/microservices';
+import { CreatePaymentDto } from './dtos/create-payment.dto';
 import { Payment } from './entities/payment.entity';
 import { PaymentService } from './payment.service';
 
 @Controller('payment')
 export class PaymentController {
   constructor(private paymentService: PaymentService) {}
-  
+
   @MessagePattern({ type: 'create-payment' })
   public async createPayment(
-    createPaymentDto: ,
+    createPaymentDto: CreatePaymentDto,
   ): Promise<Payment> {
     return await this.paymentService.createPayment(createPaymentDto);
   }
