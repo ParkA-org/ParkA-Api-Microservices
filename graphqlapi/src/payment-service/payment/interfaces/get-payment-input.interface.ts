9545e55c9ca3be863ca8d048e3fde06dad0482d7
David <davidbujosa@gmail.com>
2020-10-05 04:16:09 -0400

    Add get payment by id interface

---
+++ graphqlapi/src/payment-service/payment/interfaces/get-payment-input.interface.ts
@@ -0,0 +1,3 @@
+export interface IGetPaymentByIdInput {
+  id: string;
+}
--- graphqlapi/src/payment-service/payment/payment.service.ts
+++ graphqlapi/src/payment-service/payment/payment.service.ts
@@ -1,4 +1,46 @@
 import { Injectable } from '@nestjs/common';
+import { Client, ClientProxy, Transport } from '@nestjs/microservices';
+import { CreatePaymentInput } from './inputs/create-payment.input';
+import { DeletePaymentInput } from './inputs/delete-payment.input';
+import { PaymentType } from './types/payment.type';
 
 @Injectable()
-export class PaymentService {}
+export class PaymentService {
+  @Client({
+    transport: Transport.REDIS,
+    options: {
+      url: process.env.REDIS,
+    },
+  })
+  private client: ClientProxy;
+
+  public async createPayment(
+    createPaymentInput: CreatePaymentInput,
+  ): Promise<PaymentType> {
+    const response = await this.client.send<PaymentType>(
+      { type: 'create-payment' },
+      createPaymentInput,
+    );
+    return response.toPromise();
+  }
+
+  public async deletePayment(
+    deletePaymentInput: DeletePaymentInput,
+  ): Promise<PaymentType> {
+    const response = await this.client.send<PaymentType>(
+      { type: 'delete-payment' },
+      deletePaymentInput,
+    );
+    return response.toPromise();
+  }
+
+  public async getPaymentById(
+    getPaymentByIdInput: GetPaymentByIdInput,
+  ): Promise<PaymentType> {
+    const response = await this.client.send<PaymentType>(
+      { type: 'get-payment' },
+      getPaymentByIdInput,
+    );
+    return response.toPromise();
+  }
+}
