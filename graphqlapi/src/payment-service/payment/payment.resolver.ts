9052dd26d59174803a1c86cb346b62ec739f2995
David <davidbujosa@gmail.com>
2020-10-05 04:07:25 -0400

    Finish edit payment resolver

--- graphqlapi/src/payment-service/payment/payment.resolver.ts
+++ graphqlapi/src/payment-service/payment/payment.resolver.ts
@@ -1,21 +1,25 @@
-import {
-  BadRequestException,
-  UnauthorizedException,
-  UseGuards,
-} from '@nestjs/common';
+import { BadRequestException, Logger, UseGuards } from '@nestjs/common';
 import { Query, Resolver, Mutation, Args } from '@nestjs/graphql';
 import { AuthGuard } from 'src/auth-service/strategy/auth.guard';
 import { CreatePaymentInput } from './inputs/create-payment.input';
+import { DeletePaymentInput } from './inputs/delete-payment.input';
 import { PaymentService } from './payment.service';
 import { PaymentType } from './types/payment.type';
 @Resolver(of => PaymentType)
 export class PaymentResolver {
+  private logger = new Logger('PaymentResolver');
+
   constructor(private paymentService: PaymentService) {}
 
   @Query(returns => PaymentType)
   @UseGuards(AuthGuard)
-  deletePayment(@Args('id') id: string) {
-    return this.paymentService.deletePayment(id);
+  deletePayment(
+    @Args('deletePaymentInput') deletePaymentInput: DeletePaymentInput,
+  ) {
+    this.logger.debug(
+      `Received delete payment id data ${JSON.stringify(deletePaymentInput)}`,
+    );
+    return this.paymentService.deletePayment(deletePaymentInput);
   }
 
   @Mutation(returns => PaymentType)
@@ -23,6 +27,9 @@ export class PaymentResolver {
   async createPayment(
     @Args('createPaymentInput') createPaymentInput: CreatePaymentInput,
   ): Promise<PaymentType> {
+    this.logger.debug(
+      `Received create payment data ${JSON.stringify(createPaymentInput)}`,
+    );
     const payment = await this.paymentService.createPayment(createPaymentInput);
     if (!payment) {
       throw new BadRequestException('This payment already exists');
