b56c34abeb5ce1cd159d6d70779327d6e7bff7b4
David <davidbujosa@gmail.com>
2020-10-08 04:05:25 -0400

    Add proxy in parking service

--- graphqlapi/src/parking-service/parking.service.ts
+++ graphqlapi/src/parking-service/parking.service.ts
@@ -1,4 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import {
+  ClientProxy,
+  ClientProxyFactory,
+  Transport,
+} from '@nestjs/microservices';
 
 @Injectable()
-export class ParkingService {}
+export class ParkingService {
+  private logger = new Logger('ParkingService');
+  private client: ClientProxy;
+
+  constructor() {
+    this.client = ClientProxyFactory.create({
+      transport: Transport.REDIS,
+      options: { url: `${process.env.REDIS_URL}` },
+    });
+  }
+}
