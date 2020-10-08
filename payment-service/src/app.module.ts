77c6ef5a095bff10cb8876090735ae4683c9a5d7
Sebastiano Faiella <xeno097.cp@gmail.com>
2020-10-07 22:29:11 -0400

    added .env to git ignore file list and load payment service config from .env

--- payment-service/src/app.module.ts
+++ payment-service/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmConfig } from './config/typeorm.config';
 import { PaymentModule } from './payment/payment.module';
 import { CardModule } from './card/card.module';
+import { Card } from './card/entities/card.entity';
+import { Payment } from './payment/entities/payment.entity';
 
 @Module({
   imports: [
@@ -11,7 +12,14 @@ import { CardModule } from './card/card.module';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    TypeOrmModule.forRoot(typeOrmConfig),
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: `${process.env.MONGODB_CONNECTION_STRING}`,
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      synchronize: true,
+      entities: [Card, Payment],
+    }),
     CardModule,
   ],
   providers: [],
