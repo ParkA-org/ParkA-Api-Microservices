80c782c98ae3660b10f089d6a627661bad509441
Sebastiano Faiella <xeno097.cp@gmail.com>
2020-10-07 23:01:02 -0400

    removed dotenv from main.ts files

--- email-service/src/main.ts
+++ email-service/src/main.ts
@@ -1,9 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
 
-dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.createMicroservice(AppModule, {
     transport: Transport.REDIS,
