60495d05e1d7dedb2c2b8398dcdd1606f0c70f87
David <davidbujosa@gmail.com>
2020-10-09 08:24:31 -0400

    Finish parking resolver

--- graphqlapi/src/parking-service/parking/parking.resolver.ts
+++ graphqlapi/src/parking-service/parking/parking.resolver.ts
@@ -10,19 +10,17 @@ export class ParkingResolver {
   constructor(private parkingService: ParkingService) {}
 
   @Query(returns => ParkingType)
-  @UseGuards(AuthGuard)
-  getParkingById(@Args('id') id: string, @Context('user') user: JWTpayload) {
-    return this.parkingService.getParkingById(id, user);
+  getParkingById(@Args('id') id: string) {
+    return this.parkingService.getParkingById(id);
   }
 
-  @Query(returns => ParkingType)
+  @Query(returns => [ParkingType])
   @UseGuards(AuthGuard)
   getAllMyParkings(@Context('user') user: JWTpayload) {
     return this.parkingService.getAllMyParkings(user);
   }
 
   @Query(returns => [ParkingType])
-  @UseGuards(AuthGuard)
   getAllParkings() {
     return this.parkingService.getAllParkings();
   }
@@ -30,14 +28,28 @@ export class ParkingResolver {
   @Mutation(returns => ParkingType)
   @UseGuards(AuthGuard)
   async updateParking(
-    @Args('updateUserPasswordInput')
-    UpdateParkingInput: UpdateParkingInput,
+    @Args('updateParkingInput')
+    updateParkingInput: UpdateParkingInput,
     @Context('user') user: JWTpayload,
   ): Promise<ParkingType> {
-    const updateUser = await this.parkingService.updateParking(
-      UpdateParkingInput,
+    const updateParking = await this.parkingService.updateParking(
+      updateParkingInput,
       user,
     );
-    return updateUser;
+    return updateParking;
+  }
+
+  @Mutation(returns => ParkingType)
+  @UseGuards(AuthGuard)
+  async createParking(
+    @Args('createParkingInput')
+    createParkingInput: CreateParkingInput,
+    @Context('user') user: JWTpayload,
+  ): Promise<ParkingType> {
+    const createParking = await this.parkingService.createParking(
+      createParkingInput,
+      user,
+    );
+    return createParking;
   }
 }
