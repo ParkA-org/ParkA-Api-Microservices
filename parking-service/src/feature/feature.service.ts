b4e8bd83d0cc6deaff9acfd71f5b5be58de490b7
David <davidbujosa@gmail.com>
2020-10-09 07:46:06 -0400

    Fix feature service and format

--- parking-service/src/feature/feature.service.ts
+++ parking-service/src/feature/feature.service.ts
@@ -8,47 +8,56 @@ import { RpcException } from '@nestjs/microservices/exceptions/rpc-exception';
 
 @Injectable()
 export class FeatureService {
-
   private logger = new Logger('FeatureService');
 
   constructor(
     @InjectRepository(Feature) private featureRepository: Repository<Feature>,
   ) {}
 
-  public async createFeature(createFeatureDro: CreateFeatureDto): Promise<Feature>{  
-    const {name} = createFeatureDro;
+  public async createFeature(
+    createFeatureDro: CreateFeatureDto,
+  ): Promise<Feature> {
+    this.logger.debug(
+      `Received create feature with payload ${JSON.stringify(
+        createFeatureDro,
+      )}`,
+    );
+
+    const { name } = createFeatureDro;
+
     try {
-        const feature = this.featureRepository.save({
-          id: uuid(),
-          name,
-          createdAt: new Date().toISOString(),
-          updatedAt: new Date().toISOString(),
-        });
-  
-        return await feature;
-      } catch (error) {
-        throw error.code === 11000
-          ? new RpcException('Duplicate field')
-          : new RpcException('An undefined error occured');
-      }
+      const feature = this.featureRepository.save({
+        id: uuid(),
+        name,
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString(),
+      });
+
+      return await feature;
+    } catch (error) {
+      throw error.code === 11000
+        ? new RpcException('Duplicate field')
+        : new RpcException('An undefined error occured');
+    }
   }
-  
+
   public async getFeatureById(id: string): Promise<Feature> {
+    this.logger.debug(`Received get feature by ID`);
     try {
-        const feature = this.featureRepository.findOne({ id });
-        return await feature;
-      } catch (error) {
-        throw new RpcException('Feature not Found');
-      }
+      const feature = this.featureRepository.findOne({ id });
+      return await feature;
+    } catch (error) {
+      throw new RpcException('Feature not Found');
+    }
   }
 
-  public async getFeatures(): Promise<Feature[]>{
+  public async getAllFeatures(): Promise<Feature[]> {
+    this.logger.debug(`Received get all features`);
     try {
-        const features = this.featureRepository.find();
-        return await features;
-      } catch (error) {
-        new RpcException('Features not found');
-      }
+      const features = this.featureRepository.find();
+      return await features;
+    } catch (error) {
+      new RpcException('Features not found');
+    }
   }
-
 }
