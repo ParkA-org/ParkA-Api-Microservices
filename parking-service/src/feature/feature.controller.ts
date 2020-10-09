6fe41f8274ae495460c3a2e30e260b014309d0fe
David <davidbujosa@gmail.com>
2020-10-09 05:17:43 -0400

    Finish feature controller

--- parking-service/src/feature/feature.controller.ts
+++ parking-service/src/feature/feature.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Logger } from '@nestjs/common';
+import { MessagePattern } from '@nestjs/microservices/decorators/message-pattern.decorator';
+import { CreateFeatureDto } from './dtos/create-feature.dto';
+import { Feature } from './entities/feature.entity';
+import { FeatureService } from './feature.service';
 
 @Controller('feature')
-export class FeatureController {}
+export class FeatureController {
+  private logger = new Logger('Feature Controller');
+  constructor(private featureService: FeatureService) {}
+
+  @MessagePattern({ type: 'get-feature' })
+  public async getFeature(id: string): Promise<Feature> {
+    this.logger.debug(
+      `Received id feature message with data ${JSON.stringify(id)}`,
+    );
+    return await this.featureService.getFeature(id);
+  }
+
+  @MessagePattern({ type: 'get-features' })
+  public async getFeatures(): Promise<Feature[]> {
+    return await this.featureService.getFeatures();
+  }
+
+  @MessagePattern({ type: 'create-feature' })
+  public async createFeature(createFeatureDto: CreateFeatureDto): Promise<Feature> {
+    return await this.featureService.createFeature(createFeatureDto);
+  }
+
+
+}
