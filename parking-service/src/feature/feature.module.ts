d9bdf1e382046eea77164b094f31f20302fb242a
David <davidbujosa@gmail.com>
2020-10-09 05:13:39 -0400

    Finish feature module

--- parking-service/src/feature/feature.module.ts
+++ parking-service/src/feature/feature.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
+import { Feature } from './entities/feature.entity';
 import { FeatureController } from './feature.controller';
 import { FeatureService } from './feature.service';
 
 @Module({
   controllers: [FeatureController],
-  providers: [FeatureService]
+  providers: [FeatureService],
+  imports: [TypeOrmModule.forFeature([Feature])],
 })
 export class FeatureModule {}
