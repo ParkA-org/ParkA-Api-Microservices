f56a435c1e4d4e7a7cd1d0b81f4b43ff111f981f
David <davidbujosa@gmail.com>
2020-10-09 09:13:18 -0400

    Add intern create parking

---
+++ graphqlapi/src/parking-service/parking/interfaces/create-parking-input.interface.ts
@@ -0,0 +1,14 @@
+export interface CreateParkingInput {
+  countParking: number;
+  latitude: string;
+  longitude: string;
+  parkingName: string;
+  calendar: string[];
+  priceHours: string;
+  pictures: string[];
+  mainPicture: string;
+  sector: string;
+  direction: string;
+  information: string;
+  features: string[];
+}
