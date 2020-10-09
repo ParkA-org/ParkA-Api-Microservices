461d04c872f827b7cca59d190665549220042279
David <davidbujosa@gmail.com>
2020-10-09 05:44:36 -0400

    Finish app module in parking service

--- parking-service/src/app.module.ts
+++ parking-service/src/app.module.ts
@@ -1,12 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { ParkingModule } from './parking/parking.module';
 import { FeatureModule } from './feature/feature.module';
-
+import { Feature } from './feature/entities/feature.entity';
+import { Parking } from './parking/entities/parking.entity';
 @Module({
-  imports: [ParkingModule, FeatureModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ParkingModule,
+    FeatureModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: `${process.env.MONGODB_CONNECTION_STRING}`,
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      synchronize: true,
+      entities: [Feature, Parking],
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
