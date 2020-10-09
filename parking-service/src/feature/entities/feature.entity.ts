e3102a9b43dab034d130062dac3dc9c5ae3a4675
David <davidbujosa@gmail.com>
2020-10-09 05:08:49 -0400

    Add feature entity

--- parking-service/src/feature/entities/feature.entity.ts
+++ parking-service/src/feature/entities/feature.entity.ts
@@ -1,8 +1,22 @@
-import { Column, Entity } from "typeorm";
+import { Column, Entity, ObjectIdColumn, PrimaryColumn } from "typeorm";
+import { IBaseEntity } from "../interfaces/base-entity.interface";
 import { IFeature } from "../interfaces/feature-entity.interface";
 
 @Entity()
-export class Feature implements IFeature{
+export class Feature implements IFeature, IBaseEntity{
+
+    @ObjectIdColumn()
+    _id: string;
+
+    @PrimaryColumn()
+    id: string;
+
+    @Column()
+    updatedAt: string;
+
+    @Column()
+    createdAt: string;
+
     @Column()
     name: string;
 }
