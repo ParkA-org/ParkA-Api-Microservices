8225bb9cdf905281d71ce2833eb82187a56bf96f
David <davidbujosa@gmail.com>
2020-10-09 06:58:00 -0400

    Update Parking module

--- parking-service/src/parking/parking.module.ts
+++ parking-service/src/parking/parking.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Parking } from './entities/parking.entity';
 import { ParkingController } from './parking.controller';
 import { ParkingService } from './parking.service';
 
 @Module({
   controllers: [ParkingController],
-  providers: [ParkingService]
+  providers: [ParkingService],
+  imports: [TypeOrmModule.forFeature([Parking])],
 })
 export class ParkingModule {}
