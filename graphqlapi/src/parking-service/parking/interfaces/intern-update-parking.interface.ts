c7e370299a71c36556162beb97ce7338fbd689b0
David <davidbujosa@gmail.com>
2020-10-09 09:03:35 -0400

    Add intern update parking interface

--- graphqlapi/src/parking-service/parking/interfaces/intern-update-parking.interface.ts
+++ graphqlapi/src/parking-service/parking/interfaces/intern-update-parking.interface.ts
@@ -1,1 +1,25 @@
-export interface IInternUpdateParking {}
+export interface IInternUpdateParking {
+  id: string;
+
+  userInformation: string;
+
+  countParking: number;
+
+  published: boolean;
+
+  parkingName: string;
+
+  calendar: string;
+
+  priceHours: string;
+
+  pictures: string[];
+
+  mainPicture: string;
+
+  isAvailable: boolean;
+
+  information: string;
+
+  features: string[];
+}
