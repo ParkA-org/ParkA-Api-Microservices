fe2364559a1afe62429090c628bd4ed1bcd8487d
David <davidbujosa@gmail.com>
2020-10-09 10:13:38 -0400

    Finish feature resolver

--- graphqlapi/src/parking-service/feature/feature.module.ts
+++ graphqlapi/src/parking-service/feature/feature.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { FeatureService } from './feature.service';
 
 @Module({
-  providers: [FeatureService]
+  exports: [FeatureService],
+  providers: [FeatureService, FeatureResolver],
 })
 export class FeatureModule {}
---
+++ graphqlapi/src/parking-service/feature/feature.resolver.ts
@@ -0,0 +1,57 @@
+import { Logger } from '@nestjs/common';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { FeatureService } from './feature.service';
+import { CreateFeatureInput } from './inputs/create-feature.input';
+import { FeatureType } from './types/feature.type';
+
+@Resolver(of => FeatureType)
+export class FeatureResolver {
+  private logger = new Logger('FeatureResolver');
+
+  constructor(private featureService: FeatureService) {}
+
+  @Query(returns => FeatureType)
+  public async getFeatureById(
+    @Args('id')
+    id: string,
+  ): Promise<FeatureType> {
+    this.logger.debug(
+      `Received get feature by id with payload ${JSON.stringify(id)}`,
+    );
+
+    return this.featureService.getFeatureById(id);
+  }
+
+  @Query(returns => [FeatureType])
+  public async getFeaturesByIds(
+    @Args('ids')
+    ids: string[],
+  ): Promise<FeatureType> {
+    this.logger.debug(
+      `Received get features by ids with payload ${JSON.stringify(ids)}`,
+    );
+
+    return this.featureService.getFeaturesByIds(ids);
+  }
+
+  @Query(returns => [FeatureType])
+  public async getAllFeatures(): Promise<FeatureType[]> {
+    this.logger.debug(`Received get all features`);
+
+    return this.featureService.getAllFeatures();
+  }
+
+  @Mutation(of => FeatureType)
+  public async createFeature(
+    @Args('createFeatureInput')
+    createFeatureInput: CreateFeatureInput,
+  ): Promise<FeatureType> {
+    this.logger.debug(
+      `Received create feature with payload ${JSON.stringify(
+        createFeatureInput,
+      )}`,
+    );
+
+    return this.featureService.createFeature(createFeatureInput);
+  }
+}
