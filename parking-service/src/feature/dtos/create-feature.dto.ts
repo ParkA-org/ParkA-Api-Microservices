f2764e360f7b1b7d2b8834b074017e42318ede90
David <davidbujosa@gmail.com>
2020-10-09 05:11:51 -0400

    Add create feature dto

--- parking-service/src/feature/dtos/create-feature.dto.ts
+++ parking-service/src/feature/dtos/create-feature.dto.ts
@@ -1,5 +1,9 @@
+import { MaxLength } from "class-validator/types/decorator/string/MaxLength";
+import { MinLength } from "class-validator/types/decorator/string/MinLength";
 import { ICreatureFeatureDto } from "../interfaces/create-feature-dto.interface";
 
 export class CreateFeatureDto implements ICreatureFeatureDto{
+    @MinLength(2)
+    @MaxLength(50)
     name: string;
 }
