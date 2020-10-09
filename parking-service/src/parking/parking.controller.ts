064a185d202ab1c92e85902e0fe20013b3bddb0e
David <davidbujosa@gmail.com>
2020-10-09 06:51:49 -0400

    Fix parking controller

--- parking-service/src/parking/parking.controller.ts
+++ parking-service/src/parking/parking.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Logger } from '@nestjs/common';
+import { MessagePattern } from '@nestjs/microservices';
+import { CreateParkingDto } from './dtos/create-parking.dto';
+import { UpdateParkingDto } from './dtos/update-parking.dto';
+import { Parking } from './entities/parking.entity';
+import { ParkingService } from './parking.service';
 
 @Controller('parking')
-export class ParkingController {}
+export class ParkingController {
+    private logger = new Logger('Parking Controller');
+    constructor(private parkingService: ParkingService) {}
+  
+    @MessagePattern({ type: 'get-parking' })
+    public async getParking(id: string): Promise<Parking> {
+      this.logger.debug(
+        `Received id parking message with data ${JSON.stringify(id)}`,
+      );
+      return await this.parkingService.getParking(id);
+    }
+  
+    @MessagePattern({ type: 'get-parkings' })
+    public async getParkings(): Promise<Parking[]> {
+      return await this.parkingService.getParkings();
+    }
+  
+    @MessagePattern({ type: 'create-parking' })
+    public async createParking(createParkingDto: CreateParkingDto): Promise<Parking> {
+      return await this.parkingService.createParking(createParkingDto);
+    }
+
+    @MessagePattern({ type: 'update-parking' })
+    public async updateParking(updateParkingDto: UpdateParkingDto): Promise<Parking> {
+      return await this.parkingService.updateParking(updateParkingDto);
+    }
+  
+}
