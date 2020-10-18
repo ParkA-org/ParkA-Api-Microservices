b4b65dccbeec97c71a4f0248fb943dca0ddd3592
David <davidbujosa@gmail.com>
2020-10-18 13:12:09 -0400

    Add AuthModule in ParkingModule

--- graphqlapi/src/parking-service/parking/parking.module.ts
+++ graphqlapi/src/parking-service/parking/parking.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from 'src/auth-service/auth.module';
 import { FeatureModule } from '../feature/feature.module';
 import { ParkingResolver } from './parking.resolver';
 import { ParkingService } from './parking.service';
 
 @Module({
   exports: [ParkingService],
-  imports: [FeatureModule],
+  imports: [FeatureModule, AuthModule],
   providers: [ParkingService, ParkingResolver],
 })
 export class ParkingModule {}
