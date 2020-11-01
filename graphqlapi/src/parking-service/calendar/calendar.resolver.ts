a343760db7bc0bcd6b489b19ee46830baaff46e2
Sebastiano Faiella <xeno097.cp@gmail.com>
2020-11-01 15:48:17 -0400

    added guard to update calendar

--- graphqlapi/src/parking-service/calendar/calendar.resolver.ts
+++ graphqlapi/src/parking-service/calendar/calendar.resolver.ts
@@ -1,4 +1,6 @@
+import { UseGuards } from '@nestjs/common';
 import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { AuthGuard } from 'src/auth-service/strategy/auth.guard';
 import { CalendarService } from './calendar.service';
 import { UpdateCalendarInput } from './inputs/update-calendar.input';
 import { CalendarType } from './types/calendar.type';
@@ -7,6 +9,7 @@ import { CalendarType } from './types/calendar.type';
 export class CalendarResolver {
   constructor(private readonly calendarService: CalendarService) {}
 
+  @UseGuards(AuthGuard)
   @Mutation(of => CalendarType)
   public async updateCalendar(
     @Args('updateCalendarInput') updateCalendarInput: UpdateCalendarInput,
