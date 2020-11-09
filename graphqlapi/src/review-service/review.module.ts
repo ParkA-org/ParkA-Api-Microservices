7baba41bbd7d7cb1a5685c904d52c4dfb17e2f7b
bujosa <davidbujosa@gmail.com>
2020-11-08 23:13:49 -0400

    Fix review module

--- graphqlapi/src/review-service/review.module.ts
+++ graphqlapi/src/review-service/review.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from 'src/auth-service/auth.module';
 import { ReservationModule } from 'src/core-service/reservation/reservation.module';
 import { ParkingModule } from 'src/parking-service/parking/parking.module';
+import { ReviewResolver } from './review.resolver';
 import { ReviewService } from './review.service';
 
 @Module({
   imports: [AuthModule, ReservationModule, ParkingModule],
   controllers: [],
-  providers: [ReviewService],
+  providers: [ReviewService, ReviewResolver],
 })
 export class ReviewModule {}
