13428b69f02c5363f38df6004e1e83c43f4d4e0a
bujosa <davidbujosa@gmail.com>
2020-11-08 22:27:03 -0400

    Add review type interface

---
+++ graphqlapi/src/review-service/interfaces/review-type.interface.ts
@@ -0,0 +1,10 @@
+export interface IReviewType {
+  id: string;
+  title: string;
+  calification: number;
+  parking: string;
+  user: string;
+  reservation: string;
+  review: string;
+  type: boolean;
+}
