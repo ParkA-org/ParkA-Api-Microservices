c44e8d88b3aa6b93520a08e35491bbdfd2553863
bujosa <davidbujosa@gmail.com>
2020-11-08 22:43:38 -0400

    Add create review input

---
+++ graphqlapi/src/review-service/inputs/create-review.input.ts
@@ -0,0 +1,32 @@
+import { Field, InputType } from '@nestjs/graphql';
+import { IsDateString, IsUUID, MaxLength, MinLength } from 'class-validator';
+import { ICreateReviewInput } from '../interfaces/create-review-input.interface';
+
+@InputType('CreateReviewInput')
+export class CreateReviewInput implements ICreateReviewInput {
+  @Field()
+  @IsUUID('all')
+  user: string;
+
+  @Field()
+  @IsUUID('all')
+  parking: string;
+
+  @Field()
+  @IsUUID('all')
+  reservation: string;
+
+  @Field({ nullable: true })
+  title: string;
+
+  @Field()
+  @MinLength(2)
+  @MaxLength(256)
+  review: string;
+
+  @Field()
+  calification: number;
+
+  @Field()
+  type: boolean;
+}
