0a379fda94b404d6f85f0ca48b22e4e82f2dd230
bujosa <davidbujosa@gmail.com>
2020-11-24 02:31:06 -0400

    Add slug in feature type

--- graphqlapi/src/parking-service/feature/types/feature.type.ts
+++ graphqlapi/src/parking-service/feature/types/feature.type.ts
@@ -4,6 +4,9 @@ import { IFeatureType } from '../interfaces/feature-type.interface';
 @ObjectType('Feature')
 export class FeatureType implements IFeatureType {
   @Field()
+  slug: string;
+
+  @Field()
   name: string;
 
   @Field()
